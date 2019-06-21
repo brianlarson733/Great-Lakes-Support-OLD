44a62cc7b7f2dde31e12894762e7eac6fc3ff122
brianlarson733 <brianlarson733@gmail.com>
2019-06-21 11:16:28 -0500

    adding main class

---
+++ EulerFive.java
@@ -0,0 +1,15 @@
+/**
+/ This class will call helper methods defined in separate classes to solve a
+/ generalized form of Euler problem #5: https://projecteuler.net/problem=5
+*/
+
+public class EulerFive{
+
+	public static void main(String[] args){
+
+		//the rest of the code is tbd...
+
+	}
+
+}
+
