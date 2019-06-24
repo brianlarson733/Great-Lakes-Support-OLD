ce722384e141fed2ebffc9119f7758e74f889dbd
Nathan Roberts <nathan.roberts@liferay.com>
2019-06-24 13:41:58 -0400

    outlined numberFactorizer.java class

--- numberFactorizer.java
+++ numberFactorizer.java
@@ -1,6 +1,33 @@
 //this class will determine the factors within a provided integer
 
 public class numberFactorizer {
+    //isPrime ? - if so store to array
+
+    //set i = 2 (this needs to be an int var)
+    int lowestprime = 2
+
+    //take input and modulo (?%?) by i and check if equals (==) 0
+
+    for (0 == ((input/lowestprime); lowestprime++)
+
+    {
+        //if yes store i and (input/i)
+        //check if i is prime
+        //if yes - store to array
+
+        if (lowestprime = Prime) {
+            int lowstore = lowestprime;
+                 }
+        //check if (input/i) is prime
+        //if yes - store to array - exit class
+
+        if (input = Prime) {
+            int inputstore = (input / lowestprime)
+                    exit for loop
+                    }
 
 
+        //if no factorize again (recursion to the top of this class)
+
+    }
 }
