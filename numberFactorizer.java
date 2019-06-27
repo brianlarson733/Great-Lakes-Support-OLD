220adca7e72c4ef6232fd0364e1be71bbb2c06fa
Nathan Roberts <nathan.roberts@liferay.com>
2019-06-27 15:57:50 -0400

    updated numberFactorizier

--- numberFactorizer.java
+++ numberFactorizer.java
@@ -1,33 +1,56 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+/**
+ / This class will call helper methods defined in separate classes to solve a
+ / generalized form of Euler problem #5: https://projecteuler.net/problem=5
+
+
+ @author = Nathan Roberts
+
+ */
+
 //this class will determine the factors within a provided integer
 
 public class numberFactorizer {
-    //isPrime ? - if so store to array
 
-    //set i = 2 (this needs to be an int var)
-    int lowestprime = 2
+    public static void main(String[] args) {
+        //set i = 2 (this needs to be an int var)
+        int lowestprime = 2;
 
-    //take input and modulo (?%?) by i and check if equals (==) 0
+    public static int numberFactorizer (int lowstore int inputstore[]) {
+        //isPrime ? - if so store to array
 
-    for (0 == ((input/lowestprime); lowestprime++)
+            if (lowerBound = isPrime){
+                int inputstore = lowerBound;
+            }
 
-    {
-        //if yes store i and (input/i)
-        //check if i is prime
-        //if yes - store to array
+        //take input and modulo (?%?) by i and check if equals (==) 0
 
-        if (lowestprime = Prime) {
-            int lowstore = lowestprime;
-                 }
-        //check if (input/i) is prime
-        //if yes - store to array - exit class
+        for (0 == (lowerBound % lowestprime); lowestPrime <= upperBound; lowestprime++)
 
-        if (input = Prime) {
-            int inputstore = (input / lowestprime)
-                    exit for loop
-                    }
+        {
 
+            {
+                //if yes store i and (input/i)
+                //check if i is prime
+                //if yes - store to array
 
-        //if no factorize again (recursion to the top of this class)
+                if (lowestprime = isPrime) {
+                    int lowstore = lowestprime;
+                }
+                //check if (input/i) is prime
+                //if yes - store to array - exit class
 
+                if ((input/lowestprime) = isPrime) {
+                    int inputstore = (input / lowestprime)
+                }
+
+                //if no factorize again (recursion to the top of this class)
+
+            }
+        }
     }
 }
