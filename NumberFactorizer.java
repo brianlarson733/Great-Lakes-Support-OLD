1bee3d6aca9e48b14f74934c93e36755e2a031d3
Nathan Roberts <nathan.roberts@liferay.com>
2019-08-01 15:47:58 -0400

    Cleaned up the file and improved log

--- NumberFactorizer.java
+++ NumberFactorizer.java
@@ -24,68 +24,32 @@ public class NumberFactorizer {
         Integer upperbound = toFactor;
         ArrayList<Integer> listC = new ArrayList<Integer>();
 
-        /**Testing code - remove this block prior to using code
-        Integer lowerbound = new Integer (2);
-        Integer upperbound = new Integer (10);
-        */
-
-    //public static int numberFactorizer (int lowstore int inputstore[]) {
-        //isPrime ? - if so store to array
-        /** This can be worked out later
-            if (lowerbound = isPrime) {
-                int inputstore = lowerbound;
-            }
-
-                else {
-            */
-        //take input and modulo (?%?) by i and check if equals (==) 0
-
+        // This For loop takes the
         for (int divider = 2; divider<=upperbound; divider++) {
+             if (0 == lowerbound % lowestprime) {
 
-            for (int i = 2; i<=upperbound;i++) {
-                if (0 == lowerbound % lowestprime) {
-                    if (lowerbound < divider){
-                        System.out.println ("Lowerbound is less than divider, no data stored");
-                        System.out.println ();
-                            }
+                    //This if determines if the lowerbound is less than the divider //
+                    if (lowerbound < divider) {
+                        //System.out.println("Lowerbound is less than divider, no data stored");
+                        //System.out.println();
+                        break;
+                    }
+
+                    //This else stores the lowest prime if lowerbound % lowestprime = 0 //
                     else {
-                        /** This code is to test that the modulo function works as expected
-                        System.out.println("Lowerbound is " + lowerbound +
-                                ", Module is " + (lowerbound % lowestprime)
-                                    + ", Lowest Prime is " + lowestprime);
-                         System.out.println();*/
-
+                        System.out.println("Printing lowest prime = " + lowestprime);
                         listC.add(lowestprime);
+                        System.out.println("Printing upperbound / lowestprime = " + (upperbound / lowestprime));
+                        listC.add(upperbound / lowestprime);
                         lowerbound++;
 
-                            }
-                    } else {
-                    /**
-                     System.out.println("Lowerbound (" + lowerbound + ") is not divisible cleanly by " + lowestprime
-                            + " has been stored to the array (implement this)"); */
-                    lowerbound++;
+                    }
+                        }
 
-                                }
-
-                                 }
                 lowestprime++;
-                lowerbound = 2; //This needs to be relocated elsewhere, right now its breaking the output
+                System.out.println();
 
-                 /**
-                    else if (lowestprime = isPrime) {
-                    int lowstore = lowestprime;
-                }
-                //check if (input/i) is prime
-                //if yes - store to array - exit class
-
-                if ((input / lowestprime) = isPrime) {
-                    int inputstore = (input / lowestprime)
-                }
-
-                //if no factorize again (recursion to the top of this class)
-
-                     */
-
+            // this } is for the outer For loop
             }
 
         return listC; }
