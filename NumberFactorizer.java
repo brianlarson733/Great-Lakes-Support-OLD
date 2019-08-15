12b09e548fa6fb7b730a6339b90e320ade181a6f
Nathan Roberts <nathan.roberts@liferay.com>
2019-08-15 15:21:29 -0400

    refactored and simplified the file

--- NumberFactorizer.java
+++ NumberFactorizer.java
@@ -18,32 +18,18 @@ import java.util.ArrayList;
 public class NumberFactorizer {
 
     public static ArrayList<Integer> factorize(int toFactor) {
-        //set i = 2 (this needs to be an integer var)
         Integer lowestprime = new Integer(2);
-        Integer lowerbound = new Integer (2);
         Integer upperbound = toFactor;
         ArrayList<Integer> listC = new ArrayList<Integer>();
 
         // This For loop takes the
-        for (int divider = 2; divider<=upperbound; divider++) {
-             if (0 == lowerbound % lowestprime) {
+        for (int primedivider = 2; primedivider<=upperbound; primedivider++) {
+             if (0 == primedivider % lowestprime) {
 
-                    //This if determines if the lowerbound is less than the divider //
-                    if (lowerbound < divider) {
-                        //System.out.println("Lowerbound is less than divider, no data stored");
-                        //System.out.println();
-                        break;
-                    }
-
-                    //This else stores the lowest prime if lowerbound % lowestprime = 0 //
-                    else {
-                        System.out.println("Printing lowest prime = " + lowestprime);
-                        listC.add(lowestprime);
-                        System.out.println("Printing upperbound / lowestprime = " + (upperbound / lowestprime));
-                        listC.add(upperbound / lowestprime);
-                        lowerbound++;
-
-                    }
+                        System.out.println("Printing lowest prime = " + primedivider);
+                        listC.add(primedivider);
+                        System.out.println("Printing upperbound / primedivier = " + (upperbound / primedivider));
+                        listC.add(upperbound / primedivider);
                         }
 
                 lowestprime++;
