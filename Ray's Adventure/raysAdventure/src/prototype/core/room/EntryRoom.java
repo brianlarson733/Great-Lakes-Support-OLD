7c42a201a4ec5b2135825a4bc29b7ebb8bbc5707
Justin Mann <justin.mann@liferay.com>
2021-02-23 11:32:29 -0600

    Updated room classes

--- Ray's Adventure/raysAdventure/src/prototype/core/room/EntryRoom.java
+++ Ray's Adventure/raysAdventure/src/prototype/core/room/EntryRoom.java
@@ -1,26 +1,12 @@
-package prototype.core;
+package prototype.room;
 import java.util.*;
-import prototype.core.*;
 
 public class EntryRoom extends BasicRoom {
 
 	//Constructor fo EntryROom
-	public EntryRoom (String description, String name) {
-		super(description,name);
+	public EntryRoom (String description, String name, String entryNarration) {
+		super("The room where it all begins","Entry Room");
+		entryNarration = "Waking up from cold sleep the room around you is full of other cryocanisters. After slowly making your way out of your own canister you walk to your friends. And realize that it is empty...";
 	}
 
-	public class PrototypeEntryRoom() {
-		
-		description = "The room where it all begins";
-	}
-
-//    public void entryRoom(){
-  //      System.out.println("Waking up from cold sleep the room around you is full of other cryocanisters");
-    //    System.out.println("After slowly making your way out of your own canister you walk to your friends");
-      //  System.out.println("And realize that it is empty...");
-        //System.out.println("..");
-        //System.out.println(".");
-        //System.out.println("\r\n");
-        //basicBeing.alloyBeing();
-    //}
 }
