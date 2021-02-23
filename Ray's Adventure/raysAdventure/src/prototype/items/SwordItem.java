f3ac823ecb678ed9fe91a9f655e4e518ff1670d3
Nathan Roberts <nathan.roberts@liferay.com>
2021-02-23 11:27:26 -0600

    Added child Item classes, an item driver class, and updated the BasicItem class

---
+++ Ray's Adventure/raysAdventure/src/prototype/items/SwordItem.java
@@ -0,0 +1,10 @@
+package prototype.items;
+
+import prototype.core.BasicItem;
+
+public class SwordItem extends BasicItem {
+
+    public SwordItem(){
+        super("This is a sword", "Excaliber");
+    }
+}
