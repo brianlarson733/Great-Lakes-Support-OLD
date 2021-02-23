9f12589142ebf46431a9daf8fe152812b61af3e4
brianlarson733 <brianlarson733@gmail.com>
2021-02-23 10:39:22 -0600

    Abstracted BasicBeing, subclasses have own instance variables now

--- Ray's Adventure/raysAdventure/src/prototype/core/being/Test.java
+++ Ray's Adventure/raysAdventure/src/prototype/core/being/Test.java
@@ -2,7 +2,7 @@ public class Test {
 	public static void main(String[] args){
 		Ray ray = new Ray();
 		Alloy alloy = new Alloy();
-		ray.getDescription();
-		alloy.getDescription();
+		ray.printDescription();
+		alloy.printDescription();
 	}
 }
