ea6b75165020d6d32dc6ecbd19ac3dba1fb20f85
brianlarson733 <brianlarson733@gmail.com>
2021-02-26 11:13:46 -0600

    modified selector class, added abstract Game, began makeChoice()

--- Ray's Adventure/raysAdventure/src/prototype/being/Ray.java
+++ Ray's Adventure/raysAdventure/src/prototype/being/Ray.java
@@ -1,13 +1,38 @@
 package prototype.being;
+import java.util.ArrayList;
+
+import misc.Tools;
 import prototype.core.BasicBeing;
+import prototype.items.ConcreteBasicItem;
 import prototype.room.EntryRoom;
 
 public class Ray extends BasicBeing {
 
-	private ArrayList<BasicItem> inventory;
+	private ArrayList<ConcreteBasicItem> inventory;
 
 	public Ray(){
 		super("Ray", "Our titular hero.", 100, new EntryRoom());
 	}
+	
+	public void makeChoice() {
+		
+		int choice; 
+		
+		do {
+			choice = Tools.getWholeNumberInput();
+			if(choice ==1) {
+				System.out.println("Your name is " + getName());
+				
+			}
+			else if(choice==2) {
+				getLocation().printDescription();
+			}
+			else {
+				System.out.println("You must be confused, that isn't an option.");
+			}
+		}
+		while(choice>2);
 
+	}
+	
 }
