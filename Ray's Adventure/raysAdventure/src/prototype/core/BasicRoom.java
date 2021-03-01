1e2c3da4e947c943543ffc60a0842324064e4b58
Justin Mann <justin.mann@liferay.com>
2021-03-01 15:32:18 -0600

    updated rooms

--- Ray's Adventure/raysAdventure/src/prototype/core/BasicRoom.java
+++ Ray's Adventure/raysAdventure/src/prototype/core/BasicRoom.java
@@ -6,11 +6,17 @@ public abstract class BasicRoom {
 	//Instance Variables for BasicRoom
 	public String description;
 	public String name;
+	public ArrayList items;
+	public ArrayList beings;
+	public ArrayList doors;
 
 	//Constructor for BasicRoom
-	public BasicRoom(String description, String name) {
+	public BasicRoom(String description, String name, ArrayList items, ArrayList beings, ArrayList doors) {
 		this.description = description;
 		this.name = name;
+		this.items = items;
+		this.beings = beings;
+		this.doors = doors;
 	}
 
 	public void printDescription(){
