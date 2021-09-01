9c7a4ada3991270e9de2e05554b258fdd772f814
Nathan Roberts <nathan.roberts@liferay.com>
2021-09-01 15:02:01 -0500

    Updated the BasicBeing and Ray classes

--- Ray's Adventure/raysAdventure/src/mercury/core/beings/BasicBeing.java
+++ Ray's Adventure/raysAdventure/src/mercury/core/beings/BasicBeing.java
@@ -1,28 +1,44 @@
 package mercury.core.beings;
 
+import mercury.core.rooms.BasicRoom;
+
 public abstract class BasicBeing {
     private String name;
     private int health;
-    //private BasicRoom location;
+    private BasicRoom location;
 
+    /** Getter for Health */
     public int getHealth(){
         return this.health;
     }
 
+    /**Setter for Health */
     public void setHealth(int newHealth){
         this.health = newHealth;
     }
-    //getter method for location
 
-    //setter method for location
+    /** Getter for Location */
+    public BasicRoom getLocation (BasicRoom location){
+        return this.location;
+    }
+
+    /** Setter for Location */
+    public void setLocation (BasicRoom newLocation){
+        this.location = newLocation;
+    }
 
     //Constructors for all instance variables
 
+    /** Interact abstract method */
     abstract public void interact();
 
-    /*public int changeHealth(){
-        } */
+    //Change Health
+    public void changeHealth(int change) {
+        this.health += change;
+    }
 
     //Change location class
-
+    public void changeLocation(BasicRoom newLocation) {
+        this.location = newLocation;
+    }
 }
