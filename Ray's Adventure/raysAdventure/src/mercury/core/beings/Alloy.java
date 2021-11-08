c864038a48793d8c13e1e0f9c3b50928612a06f2
Nathan Roberts <nathan.roberts@liferay.com>
2021-11-08 15:36:08 -0600

    Updated Alloy and made a note within Ray

--- Ray's Adventure/raysAdventure/src/mercury/core/beings/Alloy.java
+++ Ray's Adventure/raysAdventure/src/mercury/core/beings/Alloy.java
@@ -15,6 +15,15 @@ public class Alloy extends BasicBeing {
 	public void interact() {
 		// TODO Auto-generated method stub
 		System.out.println("How would you like to interact with Ray?");
-		
+		System.out.println("");
+		System.out.println("Stay");
+		System.out.println("Follow");
+		System.out.println("Eat something of Ray's");
+		System.out.println("Recieve pets");
+		System.out.println("Search the room");
+
+		//Add logic for scanner input and passing user selection
+		//Add logic for each of the actions
+
 	}
 }
